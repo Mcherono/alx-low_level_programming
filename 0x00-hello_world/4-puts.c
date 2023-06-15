f9b8ba13a025a7d0a914597863d1d05550521d2c
Mcherono <mercy.cherono@aims.ac.rw>
2023-06-15 08:07:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- *main - this is where the program starts.
- *return: returns 0.
- */
+*main - this is where the program starts.
+*return: returns 0.
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
