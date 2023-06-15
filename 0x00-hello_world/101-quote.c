f26bfa1ed91008f09cbea55fcd7fe1aca042561f
Mcherono <mercy.cherono@aims.ac.rw>
2023-06-15 12:54:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,9 @@
 /**
 *main - this is where the program starts.
 *Return: return 1
+*/
 int main(void)
 {
-	write(2, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n",59);
+	write("and that piece of art is useful\"-Dora Korpar, 2015-10-19\n");
 	return (1);
 }
