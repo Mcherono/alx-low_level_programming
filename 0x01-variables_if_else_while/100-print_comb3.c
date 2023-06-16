88831becb0715e57fd60bc0c3431857919daa8b4
Mcherono <mercy.cherono@aims.ac.rw>
2023-06-16 11:17:07 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+*main - execution begins here
+*Return: returns 0
+*/
+int main(void)
+{
+	int ones;
+	int tens;
+
+	for (ones = 0; ones <= 9; ones++)
+	{
+		for (ones = 0; ones <= 9; ones++)
+		{
+			if (!((ones == tens) || (tens > ones)))
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == 9 && tens == 8))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
