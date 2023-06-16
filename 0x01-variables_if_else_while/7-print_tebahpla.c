53d822cede9acde66326332cdcd1548fb6b1c0b7
Mcherono <mercy.cherono@aims.ac.rw>
2023-06-16 09:40:51 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - execution begins
+*Return: return 0
+*/
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
