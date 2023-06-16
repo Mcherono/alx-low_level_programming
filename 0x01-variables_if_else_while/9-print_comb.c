bdf7a522862b6fc1ac01e6886bf7d86c189114a7
Mcherono <mercy.cherono@aims.ac.rw>
2023-06-16 10:59:28 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+*main - execution begins here
+*Return: returns 0 always
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
