03dc8f8bf7992f56f966ac1ac97df92f4b35086d
Mcherono <mercy.cherono@aims.ac.rw>
2023-06-16 08:26:39 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+*main - starting point of execution
+*Return: return 0
+*/
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	return (0);
+}
