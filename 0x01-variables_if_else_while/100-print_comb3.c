ef8ec560d72d4aa93c40f26916158accc01e2030
Mcherono <mercy.cherono@aims.ac.rw>
2023-06-16 11:27:42 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,18 +5,18 @@
 */
 int main(void)
 {
-	int ones;
-	int tens;
+	int ones = '0';
+	int tens = '0';
 
-	for (tens = 0; tens <= 9; tens++)
+	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (ones = 0; ones <= 9; ones++)
+		for (ones = '0'; ones <= '9'; ones++)
 		{
 			if (!((ones == tens) || (tens > ones)))
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!(ones == 9 && tens == 8))
+				if (!(ones == '9' && tens == '8'))
 				{
 					putchar(',');
 					putchar(' ');
