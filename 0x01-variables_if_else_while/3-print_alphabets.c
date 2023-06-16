8d704786c38b907ec6ef47236f462a3965a31cb5
Mcherono <mercy.cherono@aims.ac.rw>
2023-06-16 09:00:07 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+*main - program starts from here
+*Return: return 0
+*/
+int main(void)
+{
+	int a = 97;
+	int b = 65;
+
+	while (a <= 122)
+	{
+		putchar(a);
+	}
+	while (b <= 90)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
