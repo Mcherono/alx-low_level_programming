5049315c13dfe56901b60ee1294ec96cb8bd8d02
Mcherono <mercy.cherono@aims.ac.rw>
2023-06-21 09:14:50 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,7 @@ void print_times_table(int n)
 {
 	int x, y, z;
 
-	if (n >= 0 && n <= 14)
+	if (n >= 0 && n <= 15)
 	{
 		for (x = 0; x <= n; x++)
 		{
@@ -18,19 +18,26 @@ void print_times_table(int n)
 				if (z > 99)
 				{
 					_putchar(',');
-					_putchar(32);
+					_putchar(' ');
 					_putchar((z / 100) + '0');
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
 				}
-				else
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
 				{
 					if (y != 0)
 					{
 						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
+						_putchar();
+						_putchar(' ');
+						_putchar(' ');
 					}
 					_putchar(z + '0');
 				}
