5a7adeb7504c5a8f7074d0654aacb27b5d5446ed
Mcherono <mercy.cherono@aims.ac.rw>
2023-06-21 03:34:08 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,13 @@ int print_last_digit(int n)
 {
 	int l;
 
-	if (n < 0)
-	{
-		n = -n;
-        	l = n % 10;
-	}
+	l = n % 10;
+
 	if (l < 0)
 	{
-		l = -l;
+		l = l * -1;
 	}
 	_putchar(l + '0');
+
 	return (l);
 }
