4d45887409be69219221feb17a287136c66ed391
Mcherono <mercy.cherono@aims.ac.rw>
2023-06-26 15:06:43 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,46 +1,29 @@
+#include <stdio.h>
 #include "main.h"
 /**
-*rev_string - Reverse a string
-*@s: String to reverse
-*Return: Nothing
+*rev_string - update value.
+*@s: value to be evaluate.
+*Return: not.
 */
 void rev_string(char *s)
 {
-int i = 0, length;
-length = _strlen(s) - 1;
-while (length > i)
+int len = 0;
+int l = 0;
+char *y = s;
+int e = 0;
+int x;
+char n;
+
+while (*y != '\0')
 {
-swap_char(s + length, s + i);
-i++;
-length--;
+y++;
+len++;
 }
-}
-
-/**
-*_strlen - returns the length of a string
-*@s: string
-*Return: returns lenght;
-*/
-int _strlen(char *s)
+l = len - 1;
+for ( ; e < ((l / 2) + 1) ; e++)
 {
-int count, inc;
-inc = 0;
-for (count = 0; s[count] != '\0'; count++)
-inc++;
-
-return (inc);
-}
-
-/**
-*swap_char - swap two characters
-*@a: first character
-*@b: second character
-*Return: nothing
-*/
-
-void swap_char(char *a, char *b)
-{
-char tmp = *a;
-*a = *b;
-*b = tmp;
+x = (l - e);
+n = s[e];
+s[e] = s[x];
+s[x] = n;
 }
