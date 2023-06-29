4e097023421adf261002d3e313ace9484154a8f1
Mcherono <mercy.cherono@aims.ac.rw>
2023-06-29 14:50:46 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,25 @@
 #include "main.h"
+
 /**
-*_strncpy - a function that copies a string
-*@dest: pointer to destination string.
-*@src: pointer to source string.
-*@n: number of bytes to be used.
-*Return: pointer to destination string.
+*_strncpy - concatinate the values of two integers.
+*@dest: arg a
+@src: arg b
+*@n: arg n
+*Return: void.
 */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-	int byteCount;
+	int i;
 
-	for (byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
-		dest[byteCount] = src[byteCount];
-		for (; byteCount < n; byteCount++)
-			dest[byteCount] = '\0';
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+
+	dest[i] = '\0';
+
 
 	return (dest);
 }
