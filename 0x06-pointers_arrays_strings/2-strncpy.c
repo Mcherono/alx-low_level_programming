a56edaad84555c9476a17b7f0666ba5e765a799b
Mcherono <mercy.cherono@aims.ac.rw>
2023-06-29 15:01:04 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,25 +1,30 @@
 #include "main.h"
 
 /**
-*_strncpy - concatinate the values of two integers.
-*@dest: arg a
-@src: arg b
-*@n: arg n
-*Return: void.
+*_strncat - concatenates n bytes from a string to another
+*@dest: destination string
+*@src: source string
+*@n: number of bytes of str to concatenate
+*Return: a pointer to the resulting string dest
 */
-
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; src[i] != '\0' && i < n; i++)
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
 	{
-		dest[i] = src[i];
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	for (; i < n; i++)
 
 	dest[i] = '\0';
-
 
 	return (dest);
 }
