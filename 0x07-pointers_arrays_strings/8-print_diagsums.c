4f62c0f766d81df9405baab902a4d78ee658d34c
Mcherono <mercy.cherono@aims.ac.rw>
2023-07-03 11:53:38 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,24 @@
 #include "main.h"
 /**
- * set_string - function that  sets the value of a pointer to a char.
- *@s: first value -char
- *@to: second value -int
+ * print_diagsums - prints the sums of the two diagonals of a square matrix
+ * @a: pointer to start of matrix
+ * @size: width of matrix column
+ *
+ * Return: void
  */
-void set_string(char **s, char *to)
+void print_diagsums(int *a, int size)
 {
+	int i, j, p, l = 0, r = 0;
 
-	*s = to;
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - 1 - j);
+		r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
 }
