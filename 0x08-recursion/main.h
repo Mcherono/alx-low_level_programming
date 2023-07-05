ad718b37e60fe40b2509b4cd68bacfe69315c280
Mcherono <mercy.cherono@aims.ac.rw>
2023-07-05 11:42:58 -0700

    0-puts_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,6 +2,7 @@
 #define "MAIN_H"
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+void _putchar(char c);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
