43c6543d9a144c1449a72e5303af1b17ebc6d49d
Mcherono <mercy.cherono@aims.ac.rw>
2023-08-03 13:43:17 -0700

    mercy

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * binary_to_uint - converts binary numbers
- * @b: the charcter string
- * Return: returns theconverted decimal number or 0 if fail
+ * binary_to_uint - convert a binary number to an unsigned
+ * @b: char string
+ * Return: converted decimal number and  0 if an unconvertable char
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int utter, punch;
-	int timp;
+	unsigned int tal, poer;
+	int lin;
 
 	if (b == NULL)
 		return (0);
 
-	for (timp = 0; b[timp]; timp++)
+	for (lin = 0; b[lin]; lin++)
 	{
-		if (b[timp] != '0' && b[timp] != '1')
+		if (b[lin] != '0' && b[lin] != '1')
 			return (0);
 	}
 
-	for (punch = 1, utter = 0, timp--; timp >= 0; timp--, punch *= 2)
+	for (poer = 1, tal = 0, lin--; lin >= 0; lin--, poer *= 2)
 	{
-		if (b[timp] == '1')
-			utter += punch;
+		if (b[lin] == '1')
+			tal += poer;
 	}
 
-	return (utter);
+	return (tal);
 }
