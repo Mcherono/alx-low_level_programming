f08e3e033b1dc7c4abca5152b06fa58897db797c
Mcherono <mercy.cherono@aims.ac.rw>
2023-08-04 06:57:48 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,21 +1,20 @@
 #include "main.h"
-
 /**
- * set_bit - sets value of a bit to 1
- * @n: decimal number passed bymlinker
- * @index: index position to change from 0
- * Return: 1 ifsuccessful and  -1 if  failr
- */
-int set_bit(unsigned long int *n, unsigned int index)
+*set_bit - sets value of a bit to 1
+*@a: decimal number passed bymlinker
+*@index: index position to change from 0
+*Return: 1 ifsuccessful and  -1 if  failr
+*/
+int set_bit(unsigned long int *a, unsigned int index)
 {
-	unsigned long int q;
+	unsigned long int b;
 
 	if (index > 64)
 		return (-1);
 
-	for (q = 1; index > 0; index--, q *= 2)
+	for (b = 1; index > 0; index--, b *= 2)
 		;
-	*n += q;
+	*a += b;
 
 	return (1);
 }
